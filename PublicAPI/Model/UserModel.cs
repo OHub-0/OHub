f5f5f27424c7e3e7abb3c64215aae9b98dcf94fe
Diwas Dahal <diwasdahal2006@gmail.com>
2025-07-03 23:01:14 +1000

    Custome Application User

---
+++ PublicAPI/Model/UserModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace PublicAPI.Model
+{
+    public class UserModel : IdentityUser
+    {
+        [Required, MaxLength(100)]  
+        public string FirstName { get; set; }
+
+        [Required, MaxLength(56)]
+        public string MiddleName { get; set; }
+
+        [Required, MaxLength(100)]
+        public string? LastName { get; set; }
+
+        public DateTime? DateofBirth   { get; set; }
+      
+        public string? Address { get; set; }
+  
+        public string? City { get; set; }
+        public string? State { get; set; }
+        public string? Country { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+
+    }
+}
