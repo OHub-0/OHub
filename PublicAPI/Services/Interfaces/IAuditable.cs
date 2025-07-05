6f39d21b3ef5135ee63de2ca9bb9fec00211d4eb
Diwas Dahal <diwasdahal2006@gmail.com>
2025-07-05 14:16:12 +1000

    Interface for automatic timestamping

---
+++ PublicAPI/Services/Interfaces/IAuditable.cs
@@ -0,0 +1,8 @@
+﻿namespace PublicAPI.Services.Interfaces
+{
+    public interface IAuditable
+    {
+        public DateTime CreatedDate { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+    }
+}
