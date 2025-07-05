c103bc96972d2fb88e303e2c7450fce45c5528cf
Diwas Dahal <diwasdahal2006@gmail.com>
2025-07-05 14:15:53 +1000

    Institution

---
+++ PublicAPI/Model/Institution.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using PublicAPI.Services.Interfaces;
+
+namespace PublicAPI.Model
+{
+    public class Institution: IAuditable
+    {
+        [Required, MaxLength(100)]
+        public string Name { get; set; }
+        [Required]
+        public string description { get; set; }
+        public int creatorId { get; set; }
+
+        public UserModel Creator { get; set; }
+
+        public string? secondaryEmail { get; set; }
+
+        public string country { get; set; }
+        public string state { get; set; }
+
+        public string city { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+
+    }
+}
